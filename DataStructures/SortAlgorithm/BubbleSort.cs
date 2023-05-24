c9c901f9d25295b685a630c6c2c6f2216e0deaf0
hacerkrsoglu <kirisogluhacer@gmail.com>
2023-05-24 15:27:43 +0300

    Updated

---
+++ DataStructures/SortAlgorithm/BubbleSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SortAlgorithm
+{
+    public static class BubbleSort<T> where T : IComparable<T>
+    {
+
+        public static void Sort(T[] list)
+        {
+
+            bool isSorted = true;
+            for (int i = 0; i < list.Length; i++)
+            {
+                for (int j = 0; j < list.Length - i - 1; j++)
+                {
+                    if (list[j].CompareTo(list[j + 1]) > 0)
+                    {
+                        Swap(ref list[j], ref list[j + 1]);
+                        isSorted = false;
+                    }   
+                        
+
+
+                }
+                if (isSorted == true)
+                {
+                    break;
+                }
+
+            }
+                
+                   
+        }
+        private  static void Swap(ref T value, ref T value2)
+        {
+            var temp = value;
+            value = value2;
+            value2 = temp;
+        }
+
+    }
+}
